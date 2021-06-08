fa0a8636260851132d327b6eedd12d7274592d25
AndreaDex <desiderio.andrea@outlook.it>
2021-06-08 15:22:05 +0200

    add tvshow search

--- Milestone2/assets/js/main.js
+++ Milestone2/assets/js/main.js
@@ -1,19 +1,32 @@
 const app = new Vue({
   el: "#app",
   data: {
-    url: "https://api.themoviedb.org/3/search/movie",
+    url: "https://api.themoviedb.org/3",
+    movieEnd: "/search/movie",
+    tvEnd: "/search/tv",
     apiKey: "fbd2d625ea378bc924e2a3395e7ebe20",
     query: "",
     cinema: null,
+    tvShows: null,
   },
   methods: {
     searchContent() {
-      let fullUrl = `${this.url}?api_key=${this.apiKey}&query=${this.query}`;
+      let fullMovieUrl = `${this.url}${this.movieEnd}?api_key=${this.apiKey}&query=${this.query}`;
       axios
-        .get(fullUrl)
+        .get(fullMovieUrl)
         .then((response) => {
-          console.log(response.data.results);
           this.cinema = response.data.results;
+          console.log(this.cinema);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+      let fullTvUrl = `${this.url}${this.tvEnd}?api_key=${this.apiKey}&query=${this.query}`;
+      axios
+        .get(fullTvUrl)
+        .then((response) => {
+          this.tvShows = response.data.results;
+          console.log(this.tvShows);
         })
         .catch((e) => {
           console.log(e);
