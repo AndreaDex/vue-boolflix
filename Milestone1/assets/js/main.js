fd649f694d910dfe957dd93e3826e10f45658131
AndreaDex <desiderio.andrea@outlook.it>
2021-06-08 12:24:50 +0200

    complete milestone1

--- Milestone1/assets/js/main.js
+++ Milestone1/assets/js/main.js
@@ -3,11 +3,23 @@ const app = new Vue({
   data: {
     url: "https://api.themoviedb.org/3/search/movie",
     apiKey: "fbd2d625ea378bc924e2a3395e7ebe20",
-    query: "anelli",
+    query: "",
+    cinema: null,
   },
-  methods: {},
+  methods: {
+    searchContent() {
+      let fullUrl = `${this.url}?api_key=${this.apiKey}&query=${this.query}`;
+      axios
+        .get(fullUrl)
+        .then((response) => {
+          console.log(response.data.results);
+          this.cinema = response.data.results;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
   computed: {},
-  mounted() {
-    let fullUrl = `${this.url}?api_key=${this.apiKey}&query=${this.query}`;
-  },
+  mounted() {},
 });
